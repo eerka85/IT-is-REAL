10ba1ed3cde658fb04fd7d6a3ef6f55c6700686e
eerka85 <p8741129@gmail.com>
2026-05-21 08:32:49 +0200

    priklad 3

--- soub_p_3/souborovy_priklad_3.c
+++ soub_p_3/souborovy_priklad_3.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-
+int kresli_do_soub(char blud[5][5], int velikost){
+    FILE * soubor = fopen("C:/Users/Honza Stehlik/Desktop/IT is REAL/soub_p_3/data.txt", "w");
+    if(soubor == NULL){
+        return 1;
+    }
+    for(int i = 0; i<velikost; i++){
+        for(int j = 0; j<velikost; j++){
+            fprintf(soubor, "%c", blud[i][j]);
+        }
+        fprintf(soubor, "\n");
+    }
+    fclose(soubor);
+    return 0;
+}
 int main(){
     char blud[5][5] = {
         "XXXXX",
@@ -12,11 +25,12 @@ int main(){
         "XXXXX"
     };
     int velikost = sizeof(blud[1]);
-    for(int i = 0; i<velikost; i++){
-        for(int j; j<velikost; j++){
-            printf("%c", blud[i][j]);
-        }
-        printf("\n");
-    }
+    int control = kresli_do_soub(blud, velikost);
+    if(control != 0){
+        perror("jses fakt pica");
+        return 1;
+    } 
+    
     Sleep(10000);
+    return 0;
 }
