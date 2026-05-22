7165a070fdf63bfffecc07efd01aa73633ce22cb
eerka85 <p8741129@gmail.com>
2026-05-22 11:59:20 +0200

    soubor 2 dun

--- soub_p_2/soub_p_2.c
+++ soub_p_2/soub_p_2.c
@@ -1,46 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <time.h>
+#include <windows.h>
 
-void clean_buffer(){
-    int c;
-    while((c = getchar()) != '\n' && c != EOF){}
+void gen_rand_int_arr(int arr[], int velikost){
+    for(int i = 0; i< velikost; i++){
+        arr[i] = rand() % 9;
+    }
 }
-int input_string(char nacteny_str[], char vypis[]){
-    while(1){
-        printf("%s", vypis);
-        if (fgets(nacteny_str, 10, stdin) == NULL){
-            printf("\n spatne nahrany string");
-            continue;
+void printf_int_arr(int arr[], int velikost){
+    printf("\ntisk pole:\n");
+    for(int i = 0; i< velikost; i++){
+        printf("%d\n", arr[i]);
+    }
+    printf("\nKONEC tisk pole\n");
+}
+int fprint_sud_lich_arr(int arr[], int velikost){
+    //dva file pointer
+    FILE * lich_soub = fopen("C:/Users/Honza Stehlik/Desktop/IT is REAL/soub_p_2/lich_soub.txt", "w");
+    FILE * sud_soub = fopen("C:/Users/Honza Stehlik/Desktop/IT is REAL/soub_p_2/sud_soub.txt", "w");
+    if(lich_soub == NULL || sud_soub == NULL){
+        perror("chyby v otvirani souboru :(");
+        return 1;
+    }
+    //if (pole[i] %2 == 1) tak fprintf(lichej file pointer, %d) 
+    for(int i = 0; i<velikost; i++){
+        if(arr[i] %2 == 1){
+            fprintf(lich_soub,"%d", arr[i]);
         }
-        break;
+        else{
+            fprintf(sud_soub,"%d", arr[i]);
+        }
     }
-    int pozice = strcspn(nacteny_str, "\n");
-    if(nacteny_str[pozice] == '\n'){
-        nacteny_str[pozice] = '\0';
-    }
-    else{
-        clean_buffer();
-    }
+    fclose(lich_soub);
+    fclose(sud_soub);
     return 0;
 }
-int input_int(int min, int max, char vypis[]) {
-	int tmp = 0;
-	while(1) {
-		printf("%s", vypis);
-		int is_input_valid = scanf("%d", &tmp);
-		clean_buffer();
-		if(is_input_valid == 0 || tmp <min || tmp >max) {
-			printf(  "##failed to load number##\n" );
-			continue;
-		}
-		else {
-			return tmp;
-		}
-	}
-}
-
 int main(){
+    srand(time(0));
+    int velikost = 10;
+    int arr[velikost];
+    int control = 0;
 
+    gen_rand_int_arr(arr, velikost);
+    printf_int_arr(arr, velikost);
+
+    control = fprint_sud_lich_arr(arr, velikost);
+    if(control != 0){
+        perror("fprint_sud a lich se nepovedlo");
+        return 1;
+    }
     
+
+    return 0;
 }
