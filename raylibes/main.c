86ae456964101003f7fbfa06798543dadcc5ea7d
eerka85 <p8741129@gmail.com>
2026-06-05 08:47:15 +0200

    button press

--- raylibes/main.c
+++ raylibes/main.c
@@ -10,8 +10,11 @@ int main(void) {
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
     //zalozeni veci
-    float my_button_size_a = 50;
+    float my_button_size_a = 100;
+    int button_txt_font_size = 20;
     Rectangle button = {(screenWidth / 2) - (my_button_size_a /2), (screenHeight / 2) - (my_button_size_a /2), my_button_size_a, my_button_size_a};
+    
+    
 
     // Main game loop
     while (!WindowShouldClose()) { // Detect window close button or ESC key
@@ -21,7 +24,21 @@ int main(void) {
         // Draw
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawRectangle(button.x, button.y, button.width, button.height, MAROON);
+            if(CheckCollisionPointRec(GetMousePosition(), button)){
+                if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+                    ClearBackground(RAYWHITE);
+                    DrawRectangle(button.x, button.y, button.width, button.height, BLUE);
+                    DrawText("BUTTON", button.x + (my_button_size_a /2) - (MeasureText("BUTTON", button_txt_font_size) / 2), button.y  + (my_button_size_a /2) - (button_txt_font_size / 2), button_txt_font_size, YELLOW);
+                }
+                else{
+                    DrawRectangle(button.x, button.y, button.width, button.height, RED);
+                    DrawText("BUTTON", button.x + (my_button_size_a /2) - (MeasureText("BUTTON", button_txt_font_size) / 2), button.y  + (my_button_size_a /2) - (button_txt_font_size / 2), button_txt_font_size, YELLOW);
+                }
+            }
+            else{
+                DrawRectangle(button.x, button.y, button.width, button.height, GRAY);
+                DrawText("BUTTON", button.x + (my_button_size_a /2) - (MeasureText("BUTTON", button_txt_font_size) / 2), button.y  + (my_button_size_a /2) - (button_txt_font_size / 2), button_txt_font_size, YELLOW);
+            }
         EndDrawing();
     }
 
