b924ba47ab8388e391d1aa4434223e3c20ed9c8f
eerka85 <johnny.stehlik@gmail.com>
2026-06-07 12:40:25 +0200

    start menu

--- RAY_pong/main.c
+++ RAY_pong/main.c
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include <stdlib.h>
 
+void draw_rec_but_txt(Rectangle button, const char txt[], int font_siz, Color color);
+
 int main(){
     const int screenWidth_jkjk = 800; //idk jak velkou obrazovku ma kazdej
     const int screenHeight_jkjk = 450;
@@ -12,22 +14,46 @@ int main(){
     SetWindowMinSize(screenWidth, screenHeight); //jinak to da zase 800 450
     ClearWindowState(FLAG_WINDOW_RESIZABLE); //flagy jsoi lowk cool
 
-    Texture pong_logo = LoadTexture("pixilart-drawing.png");
+    Texture pong_logo = LoadTexture("pixilart-drawing.png"); //800 x 450
+
+    float rec_w = 100.0f;
+    float rec_gap = 20.0f; //20 is the gap between the PONG and button
+    Rectangle button_start = { 
+        (screenWidth / 2) - (rec_w / 2), //half of width 
+        ((screenHeight / 2) - (pong_logo.height / 2)) + pong_logo.height + rec_gap, 
+        100, // width
+        70   // height                             
+    };
+    Color color_start;
 
     int ball_radius = 10;
     float Y_ball_speed = 50.0f;
     float X_ball_speed = 50.0f;
 
     
-    while(!WindowShouldClose()){
-
+    while(!WindowShouldClose()){ //start menu
+        if(CheckCollisionPointRec(GetMousePosition(), button_start)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                break;
+            }
+            color_start = GRAY;
+        }
+        else{
+            color_start = BLUE;
+        }
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawTexture(pong_logo, 10, 10, WHITE);
+            DrawTexture(pong_logo, (screenWidth / 2) - (pong_logo.width / 2), (screenHeight / 2) - (pong_logo.height / 2), WHITE);
+            draw_rec_but_txt(button_start, "PLAY", 20, color_start);
         EndDrawing();
 
     }
 
 
     CloseWindow();
+}
+
+void draw_rec_but_txt(Rectangle button, const char txt[], int font_siz, Color color){
+    DrawRectangle(button.x, button.y, button.width, button.height, color);
+    DrawText(txt, (button.x + (button.width / 2)) - (MeasureText(txt, font_siz) /2), (button.y + (button.height / 2)) - (font_siz / 2), font_siz, RED);
 }
